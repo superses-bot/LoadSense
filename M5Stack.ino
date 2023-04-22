dc20c39346080468c3f5d0cc482d1b4354daf76b
Uday-Menon <127295526+Uday-Menon@users.noreply.github.com>
2023-04-22 11:54:45 +0400

    Update M5Stack.ino

--- M5Stack.ino
+++ M5Stack.ino
@@ -1,21 +1,28 @@
 #include <M5Core2.h>
-#define THRESHOLD_VIBRATION 6
+#define THRESHOLD_VIBRATION 8
+
 
 float gyroX = 0.0F;
 float gyroY = 0.0F;
 float gyroZ = 0.0F;
 bool available = false;
 
+
 RTC_TimeTypeDef targettime;
 RTC_TimeTypeDef currenttime;
 
+
+
+
 bool Is_shaking(float gyroX, float gyroY, float gyroZ){
   if ((gyroX > THRESHOLD_VIBRATION)||(gyroY > THRESHOLD_VIBRATION)||(gyroZ > THRESHOLD_VIBRATION)){
-    return true;
+      return true;
   }else{
     return false;
   }
 }
+
+
 
 bool Availability_check(void){
   M5.Rtc.GetTime(&currenttime); // gets and stores current time
@@ -31,46 +38,63 @@ bool Availability_check(void){
   return(true);
 }
 
+bool vibration_check(void){
+  M5.Rtc.GetTime(&currenttime); // gets and stores current time
+  targettime = currenttime;
+  targettime.Seconds = currenttime.Seconds + 1; // calculates target time using current time
+  while ((targettime.Hours*3600 + targettime.Minutes*60+targettime.Seconds) > (currenttime.Hours*3600 + currenttime.Minutes*60+ currenttime.Seconds)){ // loops for 10 minutes
+    M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+    if (Is_shaking(gyroX, gyroY, gyroZ) == false){
+      return(false); // exits loop, tells us the machine is being used
+    }
+    M5.Rtc.GetTime(&currenttime);
+  }
+  return(true);
+}
+
+
 void setup() {
   M5.begin();
   M5.IMU.Init();
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextColor(WHITE, BLACK);
   M5.Lcd.setTextSize(2);
-  
+ 
   currenttime.Hours = 0;
   currenttime.Minutes = 0;
   currenttime.Seconds = 0;
 
+
   M5.Rtc.SetTime(&currenttime);
 }
+
 
 void loop() {
   M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);  // Stores the gyroscope data to the relevant variable
 
-  if (Is_shaking(gyroX, gyroY, gyroZ) == true) {
+
+  if (vibration_check() == true) {
     M5.Lcd.setTextColor(GREEN, BLACK);
-    M5.Lcd.setCursor(120,0);
+    M5.Lcd.setCursor(100,0);
     M5.Lcd.setTextSize(2);
     M5.Lcd.printf("  VIBRATING  ");
     available = false;
   }else {
     M5.Lcd.setTextColor(RED, BLACK);
-    M5.Lcd.setCursor(120,0);
+    M5.Lcd.setCursor(100,0);
     M5.Lcd.setTextSize(2);
     M5.Lcd.printf("NOT VIBRATING");
     available = Availability_check();
   }
-  if (available){ // displays availability
+  if (available){
     M5.Lcd.setTextColor(WHITE, BLACK);
-    M5.Lcd.setCursor(120,100);
+    M5.Lcd.setCursor(100,100);
     M5.Lcd.setTextSize(2);
     M5.Lcd.printf("  AVAILABLE  ");
   }else{
     M5.Lcd.setTextColor(WHITE, BLACK);
-    M5.Lcd.setCursor(120,100);
+    M5.Lcd.setCursor(100,100);
     M5.Lcd.setTextSize(2);
     M5.Lcd.printf("NOT AVAILABLE");
   }
-
 }
